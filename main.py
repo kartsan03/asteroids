18b39859aaf8e44c38690582d502a2766d90fa8b
kartsan03 <kartsan03@gmail.com>
2026-03-28 17:15:54 +0100

    Add collisions with asteroids

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame, sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from logger import log_event
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -28,6 +29,11 @@ def main():
 				return
 		screen.fill("black")
 		updatable.update(dt)
+		for asteroid in asteroids:
+			if asteroid.collides_with(player):
+				log_event("player_hit")
+				print("Game over!")
+				sys.exit()
 		for obj in drawable:
 			obj.draw(screen)
 		pygame.display.flip()
