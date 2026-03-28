8838fc6d290e5c35aacc3a39724273eda7aec026
kartsan03 <kartsan03@gmail.com>
2026-03-28 16:09:59 +0100

    add pygame groups and player loop updates

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	dt = 0
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
@@ -18,7 +21,9 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		player.draw(screen)
+		updatable.update(dt)
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
