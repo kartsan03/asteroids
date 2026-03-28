66388889bba6c70a0c0af1df30a3974e5d3baad0
kartsan03 <kartsan03@gmail.com>
2026-03-28 19:15:56 +0100

    add destruction and split

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def main():
 				log_event("player_hit")
 				print("Game over!")
 				sys.exit()
+			for shot in shots:
+				if asteroid.collides_with(shot):
+					log_event("asteroid_shot")
+					shot.kill()
+					asteroid.split()
 		for obj in drawable:
 			obj.draw(screen)
 		pygame.display.flip()
